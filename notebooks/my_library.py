c8dfe7e740aadd3002d9315d1a926298c0e6b67e
jpcunhadias <joaopedrocdias@hotmail.com>
2023-02-03 22:18:41 -0300

    adding notebook EDA for itens

--- notebooks/my_library.py
+++ notebooks/my_library.py
@@ -1,25 +1,30 @@
 import nltk
+import pandas as pd
+
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 from nltk import pos_tag
 from collections import Counter
 
 
-def get_most_common_nouns(df, column_name) -> list:
+# resources for this function: https://www.nltk.org/book/ch05.html
+
+def get_most_common_nouns(df: pd.DataFrame,
+                          column_name: str,
+                          value: int) -> list:
     """
     The get_most_common_nouns function takes a dataframe and column name as input.
-    It then extracts nouns from the text in that column, flattens the list of verbs into a single list,
-    and counts the frequency of each verb. It returns this most common verbs as an output.
+    It then extracts nouns from the text in that column, flattens the list of nouns into a single list,
+    and counts the frequency of each noun. It returns this most common nouns as an output.
 
     :param df: Pass in the dataframe that contains the text
     :param column_name: Specify the column name of the text data
-    :return: A list of tuples containing the most common verbs and their frequencys
+    :param value: Specify the number of most common nouns to return
+    :return: A list of tuples containing the most common nouns and their frequencies
     :doc-author: Trelent
     """
 
-    def extract_nouns(text):
-
-
+    def extract_nouns(text: str) -> list:
         words = nltk.word_tokenize(text)
         tagged = pos_tag(words)
 
@@ -28,15 +33,15 @@ def get_most_common_nouns(df, column_name) -> list:
         return verbs
 
     # Apply the function to the text column
-    df['verbs'] = df[column_name].apply(extract_nouns)
+    df['nouns'] = df[column_name].apply(extract_nouns)
 
     # Flatten the list of verbs into a single list
-    flat_list = [item for sublist in df['verbs'].tolist() for item in sublist]
+    flat_list = [item for sublist in df['nouns'].tolist() for item in sublist]
 
     # Count the frequency of each verb
-    verb_counts = Counter(flat_list)
+    nouns_counts = Counter(flat_list)
 
-    # Get the most common verbs
-    most_common_verbs = verb_counts.most_common()
+    # Get the most common nouns
+    most_common_nouns = nouns_counts.most_common(value)
 
-    return most_common_verbs
+    return most_common_nouns
--- tests/test_my_library.py
+++ tests/test_my_library.py
@@ -1,13 +1,14 @@
-# tests/test_my_library.py
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../notebooks')))
-
 import pandas as pd
 from notebooks.my_library import get_most_common_nouns
 
+# tests/test_my_library.py
+import sys
+import os
 
-def test_get_most_common_nouns() -> list:
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../notebooks')))
+
+
+def test_get_most_common_nouns() -> None:
     text_list = ["John runs every morning.", "Mary walks in the park.", "Jane sings in the shower."]
     df = pd.DataFrame({'text': text_list})
 
@@ -16,8 +17,3 @@ def test_get_most_common_nouns() -> list:
     result = get_most_common_nouns(df, 'text')
 
     assert result == expected_result  # add assertion here
-
-
-class MyTestCase():
-    pass
-
